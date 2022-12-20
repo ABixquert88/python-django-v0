a2d0cdd9f62367997abe4e70dfdacf513ff1de4c
Alberto Bixquert Fornes <alberto7_1988@hotmail.com>
2022-12-20 15:07:21 +0100

    first commit

---
+++ autenticacion/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+
+from .views import VRegistro, cerrar_sesion,logear
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    
+
+    path('',VRegistro.as_view(), name="Autenticacion"),
+    path('cerrar_sesion',cerrar_sesion, name="cerrar_sesion"),
+    path('logear',logear, name="logear"),
+]
